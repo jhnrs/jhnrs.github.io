73ce718016b0e07112cd5a5874501021eaaabc30
john <johnrosales933@gmail.com>
2025-09-30 15:14:42 +0800

    added some animations and tweaks to css

--- content.js
+++ content.js
@@ -13,3 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // For now, do nothing
 });
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* const card = document.querySelector('.about, .currently-learning, .interests, .projects, .goals, .certifications, .skills, .contact');
+card.classList.add('show');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+const cards = document.querySelectorAll('.about, .currently-learning, .interests, .projects, .goals, .certifications, .skills, .contact')
+cards.forEach((card) => {
+  observer.observe(card);
+}); */
+
+
+const cards = document.querySelectorAll('.header, .about, .currently-learning, .interests, .projects, .goals, .certifications, .skills, .contact');
+cards.forEach((card, index) => {
+  card.style.transitionDelay = '${index * 0.5s}';
+  card.classList.add('show');
+});
